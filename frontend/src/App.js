21974371d5328c01b6dfbff62aa0665aba578b8f
Karl Andrei Abriz <beingkarl10@gmail.com>
2026-07-25 10:31:56 +0800

    feat: add Marketplace browse page with buy flow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Transfer from "./pages/Transfer";
+import Marketplace from "./pages/Marketplace";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/transfer" element={<Transfer />} />
+        <Route path="/marketplace" element={<Marketplace />} />
       </Routes>
     </BrowserRouter>
   );
