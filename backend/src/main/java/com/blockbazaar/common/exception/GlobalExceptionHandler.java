cd3ae61b493f45e608f3bace11691b206525c87a
Gyle M. Amihan <gyle.amihan@cit.edu>
2026-07-26 14:11:53 +0800

    Fix Whitelabel error page for 404s and 405s
    - Add throw-exception-if-no-handler-found and disable resource mappings in application.properties
    - Add NoHandlerFoundException handler (404 JSON response)
    - Add HttpRequestMethodNotSupportedException handler (405 JSON response)

--- backend/src/main/java/com/blockbazaar/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/blockbazaar/common/exception/GlobalExceptionHandler.java
@@ -2,15 +2,31 @@ package com.blockbazaar.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNoHandlerFound(NoHandlerFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Endpoint not found: " + ex.getRequestURL());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Map<String, String>> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Method not allowed: " + ex.getMethod());
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(response);
+    }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Map<String, String>> handleNotFoundException(NotFoundException ex) {
